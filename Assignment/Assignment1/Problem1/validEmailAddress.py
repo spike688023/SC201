931581fb66955d9b98e173bd61a02ca3572f7bdf
Spike Lin <spike688023@gmail.com>
2025-02-23 12:28:29 +0800

    update

--- Assignment/Assignment1/Problem1/validEmailAddress.py
+++ Assignment/Assignment1/Problem1/validEmailAddress.py
@@ -28,73 +28,85 @@ DATA_FILE = 'is_valid_email.txt'     # This is the file name to be processed
 
 
 def main():
-	maybe_email_list = read_in_data()
-	for maybe_email in maybe_email_list:
-		feature_vector = feature_extractor(maybe_email)
-		# TODO:
+    maybe_email_list = read_in_data()
+    wrong_case = 0
+    for index, maybe_email in enumerate(maybe_email_list):
+        feature_vector = feature_extractor(maybe_email)
+        score = 0
+        for i in range(len(feature_vector)):
+            score += feature_vector[i] * WEIGHT[i][0]
+
+        if index < 13 and score > 0:
+            wrong_case += 1
+            print("{2}:{0}: {1}, {3}, {4}".format( maybe_email, score, index+1, feature_vector, "wrong case") )
+        elif index >= 13 and score <= 0:
+            wrong_case += 1
+        print("{2}:{0}: {1}, {3}, {4}".format( maybe_email, score, index+1, feature_vector, "pass case") )
+
+
+    print("\nwrong_case : {0}".format( wrong_case ) )
+    print("len(maybe_email_list) : {0}".format( len(maybe_email_list) ) )
+    print("Accuracy of this model: {0} ".format( round( wrong_case/len(maybe_email_list) , 16)) )
 
 
 def feature_extractor(maybe_email):
-	"""
-	:param maybe_email: str, the string to be processed
-	:return: list, feature vector with 10 values of 0's or 1's
-	"""
-	feature_vector = [0] * len(WEIGHT)
-	for i in range(len(feature_vector)):
+    """
+    :param maybe_email: str, the string to be processed
+    :return: list, feature vector with 10 values of 0's or 1's
+    """
+    feature_vector = [0] * len(WEIGHT)
+    #print("len(feature_vector): {0}".format( len(feature_vector) ) )
+    for i in range(len(feature_vector)):
         # '@' in the str
-		if i == 0:
-			feature_vector[i] = 1 if '@' in maybe_email else 0
+        if i == 0:
+            feature_vector[i] = 1 if '@' in maybe_email else 0
         # No '.' before '@'
-		elif i == 1:
-			if feature_vector[0]:
-				feature_vector[i] = 1 if '.' not in maybe_email.split('@')[0] else 0
+        elif i == 1:
+            if feature_vector[0]:
+                feature_vector[i] = 1 if '.' not in maybe_email.split('@')[0] else 0
         # some strings before '@'
-		elif i == 2: 
-			if feature_vector[1]:
-				feature_vector[i] = 1 if maybe_email.split('@')[0] else 0
-        # some string after '@'
-		elif i == 3:
-			if feature_vector[2]:
-				feature_vector[i] = 1 if maybe_email.split('@')[1] else 0
+        elif i == 2: 
+            if feature_vector[0]:
+                feature_vector[i] = 1 if maybe_email.split('@')[0] else 0
+        # some strings after '@'
+        elif i == 3:
+            if feature_vector[0]:
+                feature_vector[i] = 1 if len("".join(maybe_email.split('@')[1:])) else 0
         # There is '.' after '@'
-		elif i == 4:
-			if feature_vector[3]:
-				feature_vector[i] = 1 if '.' in  "".join(maybe_email.split('@')[1:])  else 0
+        elif i == 4:
+            if feature_vector[0]:
+                feature_vector[i] = 1 if '.' in  "".join(maybe_email.split('@')[1:])  else 0
+
         # There is no white space
-		elif i == 5:
-			if feature_vector[4]:
-				feature_vector[i] = 1 if ' ' not in maybe_email else 0
+        elif i == 5:
+            feature_vector[i] = 1 if ' ' not in maybe_email else 0
         # Ends with '.com'
-		elif i == 6:
-			if feature_vector[5]:
-				feature_vector[i] = 1 if len(eq maybe_email) >= 4 and ".com" eq maybe_email[-4:] else 0
+        elif i == 6:
+            feature_vector[i] = 1 if  len(maybe_email) >= 4 and ".com" == maybe_email[-4:] else 0
         # Ends with '.edu'
-		elif i == 7:
-			if feature_vector[6]:
-				feature_vector[i] = 1 if len(eq maybe_email) >= 4 and ".edu" eq maybe_email[-4:] else 0
+        elif i == 7:
+            feature_vector[i] = 1 if len(maybe_email) >= 4 and ".edu" == maybe_email[-4:] else 0
         # Ends with '.tw'
-		elif i == 8:
-			if feature_vector[7]:
-				feature_vector[i] = 1 if len(eq maybe_email) >= 3 and ".edu" eq maybe_email[-3:] else 0
+        elif i == 8:
+            feature_vector[i] = 1 if len(maybe_email) >= 3 and ".tw" == maybe_email[-3:] else 0
         # Length > 10
-		elif i == 9:
-			if feature_vector[8]:
-				feature_vector[i] = 1 if len(eq maybe_email) > 10 else 0
-	return feature_vector
+        elif i == 9:
+            feature_vector[i] = 1 if len(maybe_email) > 10 else 0
+    return feature_vector
 
 
 def read_in_data():
-	"""
-	:return: list, containing strings that might be valid email addresses
-	"""
-	# TODO:
+    """
+    :return: list, containing strings that might be valid email addresses
+    """
+    # TODO:
     mail_list = list()
     with open(DATA_FILE, "r", encoding="utf-8") as file:
-        for line.strip() in file:  
-            mail_list.append( line )
+        for line in file:  
+            mail_list.append( line.strip() )
 
     return mail_list
 
 
 if __name__ == '__main__':
-	main()
+    main()
