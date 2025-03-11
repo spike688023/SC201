3f18741e88629f6fff5542631a8147d342dfdc4f
Lin Spike <linspike@Lins-Air.home>
2025-03-11 13:01:39 +0800

    update

--- Assignment3/titanic_level1.py
+++ Assignment3/titanic_level1.py
@@ -23,11 +23,73 @@ def data_preprocess(filename: str, data: dict, mode='Train', training_data=None)
 						  (You will only use this when mode == 'Test')
 	:return data: dict[str: list], key is the column name, value is its data
 	"""
-	############################
-	#                          #
-	#          TODO:           #
-	#                          #
-	############################
+		
+	with open(filename, 'r') as file:
+	    # first row
+        # PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
+		first_line, lines = file.readlines()[0], file.readlines()[1:]
+
+		for line in file:
+    		line = line.strip()
+			person_data = line.split(',')
+    		ans = []
+
+			# start from column Pclass
+			if mode == 'train':
+				start = 2
+			else:
+				start = 1
+
+        	# Name : index 3,4
+        	# skip NaN case in index start+4 (Age) and index start+10 (Embarked)
+			if person_data[start+4] == "" or person_data[start+10] == "":
+					continue
+
+            # add Survived info for train 
+			if mode == 'train':
+				data.setdefault("Survived", []).append( int(person_data[1]) )
+
+			# Train data
+            # 0          ,1       ,2     ,3 4 ,5  ,6  ,7    ,8    ,9     ,10  ,11   ,12
+            # PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
+            #
+			# Test data
+            # 0          ,1     ,2 3 ,4  ,5  ,6    ,7    ,8     ,9   ,10   ,11
+            # PassengerId,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
+			for i in range(len(person_data)):
+				if i == start:
+					# Pclass, normalize from 0 to 1
+				    data.setdefault("Pclass", []).append( int(person_data[i])-1) / (3-1) )
+				elif i == start+3:
+					# Sex
+					if data_lst[i] == 'male':
+				        data.setdefault("Sex", []).append( 1 )
+					else:
+				        data.setdefault("Sex", []).append( 0 )
+				elif i == start+4:
+					# Age
+					if data_lst[i]:
+						ans.append( (float(data_lst[i]) - 0.42) / (80-0.42) )
+						ans.append( ((float(data_lst[i]) - 0.42) / (80-0.42)) **2 )
+					else:
+						ans.append( (29.699 - 0.42) / (80-0.42) )
+						ans.append( ((29.699 - 0.42) / (80-0.42)) **2 )
+				elif i == start+5:
+					# SibSp, normalize from 0 to 1
+				    data.setdefault("SibSp", []).append( (int(person_data[i]) - 0) / 8 )
+				elif i == start+6:
+					# Parch, normalize from 0 to 1
+				    data.setdefault("SibSp", []).append( (int(person_data[i]) - 0) / 6 )
+				elif i == start+8:
+					# Fare
+					if person_data[i]:
+						ans.append((float(person_data[i]) - 0) / 512.3292)
+						ans.append(((float(person_data[i]) - 0) / 512.3292) **2)
+					else:
+						ans.append( 32.2/512.3292)
+						ans.append( (32.2/512.3292) **2)
+			if mode == 'train':
+				return ans, y
 	return data
 
 
