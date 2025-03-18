26f7886d033f4518e3bfa2e3079f74f80bf930d4
Lin Spike <linspike@Lins-MacBook-Air.local>
2025-03-18 13:43:35 +0800

    update

--- Assignment3/boston_housing_competition.py
+++ Assignment3/boston_housing_competition.py
@@ -12,38 +12,130 @@ model from scratch! Go data scientists!
 """
 
 import pandas as pd
-from sklearn import preprocessing, linear_model
 import numpy as np
 import matplotlib.pylab as plt
-from sklearn import metrics
+from sklearn import preprocessing, linear_model, metrics, ensemble, svm
+from sklearn.model_selection import train_test_split
 
 TRAIN_FILE = 'boston_housing/train.csv'
 TEST_FILE = 'boston_housing/test.csv'
 
 def main():
 	train_data = pd.read_csv(TRAIN_FILE)
+	test_data = pd.read_csv(TEST_FILE)
 	y = train_data.pop("medv")
 
+	X_train, X_val, y_train, y_val = train_test_split(train_data, y, test_size=0.8, random_state=42)
+
+	print( "                                     train data,   validation data,      test data predictions" )
 	#############################
 	# Degree 1 Polynomial Model #
 	#############################
 	h = linear_model.LinearRegression()
-	regressor_degree_1 = h.fit(train_data, y)
-	predictions = h.predict(train_data)
+
+	h.fit(X_train, y_train)
+
+	predictions_train = h.predict(X_train)
+	predictions_val = h.predict(X_val)
+	predictions_test = h.predict(test_data)
+	out_file_name = "linear_regression_degree_1.csv"
 
 	# RMS error
-	print( "Linear regression of poly degree 1 : ", metrics.mean_squared_error(predictions, y)**0.5 )
+	print_prediction_status( "Linear regression of poly degree 1", predictions_train, y_train, predictions_val, y_val, out_file_name)
+	out_file(test_data.ID, predictions_test, out_file_name)
 
 	#############################
 	# Degree 2 Polynomial Model #
 	#############################
 	poly = preprocessing.PolynomialFeatures(degree=2)  # 轉換成二次多項式特徵
-	X_train_poly = poly.fit_transform(train_data)
-	regressor_degree_2 = h.fit(X_train_poly, y)
-	predictions = h.predict(X_train_poly)
+	X_train_poly = poly.fit_transform(X_train)
+	X_val_poly = poly.fit_transform(X_val)
+	X_test_poly = poly.fit_transform(test_data)
+
+	h.fit(X_train_poly, y_train)
+
+	predictions_train = h.predict(X_train_poly)
+	predictions_val = h.predict(X_val_poly)
+	predictions_test = h.predict(X_test_poly)
+	out_file_name = "linear_regression_degree_2.csv"
 
 	# RMS error
-	print( "Linear regression of poly degree 2 : ", metrics.mean_squared_error(predictions, y)**0.5 )
+	print_prediction_status( "Linear regression of poly degree 2", predictions_train, y_train, predictions_val, y_val, out_file_name)
+	out_file(test_data.ID, predictions_test, out_file_name)
+
+	#############################
+	# Random Forest Regressor  #
+	#############################
+	h = ensemble.RandomForestRegressor(n_estimators=100, random_state=42)
+
+	h.fit(X_train, y_train)
+
+	predictions_train = h.predict(X_train)
+	predictions_val = h.predict(X_val)
+	predictions_test = h.predict(test_data)
+	out_file_name = "random_forest_regressor.csv"
+
+	# RMS error
+	print_prediction_status( "Random Forest Regressor", predictions_train, y_train, predictions_val, y_val, out_file_name)
+	out_file(test_data.ID, predictions_test, out_file_name)
+
+	#############################
+	# Gradient Boosting Decision Tree, GBDT #
+	#############################
+	h = ensemble.GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=4, random_state=42)
+
+	h.fit(X_train, y_train)
+
+	predictions_train = h.predict(X_train)
+	predictions_val = h.predict(X_val)
+	predictions_test = h.predict(test_data)
+	out_file_name = "gradient_boosting_decision_tree.csv"
+
+	# RMS error
+	print_prediction_status( "Gradient Boosting Decision Tree ", predictions_train, y_train, predictions_val, y_val, out_file_name)
+	out_file(test_data.ID, predictions_test, out_file_name)
+
+	#############################
+	# Support Vector Regression, SVR #
+	#############################
+	scaler_X = preprocessing.StandardScaler()
+	scaler_y = preprocessing.StandardScaler()
+	X_train_scaled = scaler_X.fit_transform(X_train)
+	y_train_scaled = scaler_y.fit_transform(np.array(y_train).reshape(-1, 1)).ravel()
+	X_val_scaled = scaler_X.transform(X_val)
+	X_test_scaled = scaler_X.transform(test_data)
+
+	#  訓練 Support Vector Regression（SVR）
+	h = svm.SVR(kernel='rbf', C=100, epsilon=0.1)
+	h.fit(X_train_scaled, y_train_scaled)
+
+	#  預測房價（需還原縮放）
+	y_train_pred_scaled = h.predict(X_train_scaled)
+	y_val_pred_scaled = h.predict(X_val_scaled)
+	y_test_pred_scaled = h.predict(X_test_scaled)
+	predictions_train = scaler_y.inverse_transform(y_train_pred_scaled.reshape(-1, 1)).ravel()
+	predictions_val = scaler_y.inverse_transform(y_val_pred_scaled.reshape(-1, 1)).ravel()
+	predictions_test = scaler_y.inverse_transform(y_test_pred_scaled.reshape(-1, 1)).ravel()
+	out_file_name = "support_vector_regression.csv"
+
+	# RMS error
+	print_prediction_status( "Support Vector Regression ", predictions_train, y_train, predictions_val, y_val, out_file_name)
+	out_file(test_data.ID, predictions_test, out_file_name)
+
+def print_prediction_status( model_str, predictions_train, y_train, predictions_val, y_val, out_file_name):
+	print( "{0:<34} : {1:<10},   {2:<8},           {3:<8}".format(model_str, round( metrics.mean_squared_error(predictions_train, y_train)**0.5 ,8), round( metrics.mean_squared_error(predictions_val, y_val)**0.5,8) , out_file_name ))
+
+def out_file(ID, predictions, filename):
+	"""
+	: param predictions: numpy.array, a list-like data structure that stores 0's and 1's
+	: param filename: str, the filename you would like to write the results to
+	"""
+
+	with open(filename, 'w') as out:
+		out.write('ID,medv\n')
+		for index in range(len(predictions)):
+			out.write(str(ID[index])+','+str(predictions[index])+'\n')
+
 
 def sign(x):
 	if x > 0:
@@ -52,6 +144,7 @@ def sign(x):
 		return -1
 	else:
 		return 0
+
 
 
 if __name__ == '__main__':
